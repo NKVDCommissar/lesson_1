1da2a7f8736d036d0872ed933fab06078a1f754a
NKVDCommissar <136933144+NKVDCommissar@users.noreply.github.com>
2023-06-28 23:12:00 +0300

    Delete 17_06_23.py

--- 17_06_23.py
+++
@@ -1,9 +0,0 @@
-def strcounter(string):
-    dict = {}
-    for i in string:
-        dict[i] = dict.get(i, 0) + 1
-    for i, cnt in dict.items():
-        print(i, cnt)
-
-string = "rarchik"
-strcounter(string)
